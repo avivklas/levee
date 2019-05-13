20abe53ae3b92c17d37e2433627ef91e7a7c061f
aviv <avivklas@gmail.com>
2019-05-13 12:22:03 +0300

    added usage

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # levee
+
+## Usage
+
+#### Create a limited reader
+```go
+now := time.Now()
+// creating new bucket with the rate of of 8/s
+localBucket := NewBucket(8/1, 8, now)
+// creating new bucket with the rate of of 64/s
+globalBucket := NewBucket(64/1, 64, now)
+// wrap an io.Reader with both buckets and get a new reader
+// that will assure no overflow above the requested bandwidth
+limiter := LimitedReader(reader, localBucket, globalBucket)
+```
--- levee.go
+++ levee.go
@@ -9,16 +9,16 @@ import (
 // levee implements io.Reader by wrapping another reader and limits
 // its bandwidth by rate limits that are defined by the bucket and the global bucket
 type levee struct {
-	r            io.Reader
+	reader       io.Reader
 	bucket       *Bucket
 	globalBucket *Bucket
 }
 
-// LimitedReader returns a levee reader that limits
-// its bandwidth by rate limits that are defined by the bucket and the global bucket
-func LimitedReader(r io.Reader, bucket *Bucket, globalBucket *Bucket) io.Reader {
+// LimitedReader returns a levee reader that limits the bandwidth of reads
+// of the underlying reader by rate limited buckets - local and global
+func LimitedReader(reader io.Reader, bucket *Bucket, globalBucket *Bucket) io.Reader {
 	return &levee{
-		r:            r,
+		reader:       reader,
 		bucket:       bucket,
 		globalBucket: globalBucket,
 	}
@@ -26,7 +26,7 @@ func LimitedReader(r io.Reader, bucket *Bucket, globalBucket *Bucket) io.Reader 
 
 // Read implementation of io.Reader
 func (r *levee) Read(buf []byte) (int, error) {
-	n, err := r.r.Read(buf)
+	n, err := r.reader.Read(buf)
 	if n <= 0 {
 		return n, err
 	}
